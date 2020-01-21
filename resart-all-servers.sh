d50b318c19d402851a24d071e433521d9c260098
Amanpreet Singh <support@whmcssmarters.com>
2020-01-21 19:28:48 +0530

    added openvpn startup services

--- resart-all-servers.sh
+++ resart-all-servers.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 # Created by WHMCS-Smarters www.whmcssmarters.com
 ipsec restart
+# Don't modify package-provided service
+cp /lib/systemd/system/openvpn\@.service /etc/systemd/system/openvpn\@.service
+
+# Workaround to fix OpenVPN service on OpenVZ
+sed -i 's|LimitNPROC|#LimitNPROC|' /etc/systemd/system/openvpn\@.service
+# Another workaround to keep using /etc/openvpn/
+sed -i 's|/etc/openvpn/server|/etc/openvpn|' /etc/systemd/system/openvpn\@.service
+
+systemctl daemon-reload
+systemctl restart openvpn@server
+systemctl enable openvpn@server
