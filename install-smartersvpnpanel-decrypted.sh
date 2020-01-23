1e30eeabfe1f84127f999cfe07bc8cd71685ae7b
amarsinghnewspark <40065362+amarsinghnewspark@users.noreply.github.com>
2020-01-23 12:45:15 +0530

    Update install-smartersvpnpanel-decrypted.sh

--- install-smartersvpnpanel-decrypted.sh
+++ install-smartersvpnpanel-decrypted.sh
@@ -159,6 +159,7 @@ bigecho "Database Created / User Creatd / Configuration Updated"
  mysql -u $MYSQLUSER $MYSQLDB -e "UPDATE tbladdonmodules SET value = '$LICENSE' WHERE module = 'vpnpanel' AND setting = 'license'";
  mysql -u $MYSQLUSER $MYSQLDB -e "UPDATE tbladdonmodules SET value = '' WHERE module = 'vpnpanel' AND setting = 'localkey'";
  mysql -u $MYSQLUSER $MYSQLDB -e "UPDATE server_list SET mainserver = 0 WHERE 1";
+ mysql -u $MYSQLUSER $MYSQLDB -e "DELETE FROM server_list WHERE server_ip = '$PUBLIC_IP'";
  mysql -u $MYSQLUSER $MYSQLDB -e "INSERT INTO server_list(server_name, flag, server_ip, server_category, sshport, server_port, pskkey, mainserver, sshpass, status,createdUploaded) VALUES ('Main Server','$DOMAIN/modules/addons/vpnpanel/assets/flags/png/no_flag.png','$PUBLIC_IP','openvpn','$SSHPORT','$VPNPORT','$SERVICEID',1,'$SSHPASS',1,'Created')";
 
 }
