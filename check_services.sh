95d63f1ced2f6a7206e994bfe605c9e60d6af56b
Amanpreet Singh <support@whmcssmarters.com>
2023-03-28 16:51:09 +0530

    Update check_services.sh

--- check_services.sh
+++ check_services.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#create folders 
-sudo mkdir -p /var/www/
+
 # Define file paths for OpenVPN and IKEv2 status
 OPENVPN_STATUS_FILE="/var/www/status/openvpn.txt"
 IKEV2_STATUS_FILE="/var/www/status/ikev2.txt"
@@ -17,20 +16,4 @@ if systemctl is-active strongswan.service > /dev/null 2>&1; then
   echo "online" > $IKEV2_STATUS_FILE
 else
   echo "offline" > $IKEV2_STATUS_FILE
-fi
-
-# Define function to restart services
-function restart_services {
-  systemctl restart openvpn.service
-  systemctl restart strongswan.service
-}
-
-# Check if services need to be restarted
-if [[ "$(cat $OPENVPN_STATUS_FILE)" == "offline" || "$(cat $IKEV2_STATUS_FILE)" == "offline" ]]; then
-  restart_services
-  if [[ "$(cat $OPENVPN_STATUS_FILE)" == "online" && "$(cat $IKEV2_STATUS_FILE)" == "online" ]]; then
-    echo "success"
-  else
-    echo "failed"
-  fi
 fi
