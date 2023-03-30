3aa8cf2bc765a7510bbb4b52fb3b1075fc862af1
Amanpreet Singh <support@whmcssmarters.com>
2023-03-30 10:13:49 +0530

    files direcs added
    creating files and directories if not found

--- check_services.sh
+++ check_services.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 # Define file paths for OpenVPN and IKEv2 status
-OPENVPN_STATUS_FILE="/var/www/status/openvpn.txt"
-IKEV2_STATUS_FILE="/var/www/status/ikev2.txt"
+if [ -e /var/www/status/openvpn.txt ]
+then
+   OPENVPN_STATUS_FILE="/var/www/status/openvpn.txt"
+else
+    mkdir -p /var/www/status/
+    touch /var/www/status/openvpn.txt
+    OPENVPN_STATUS_FILE="/var/www/status/openvpn.txt"
+fi
+
+if [ -e /var/www/status/ikev2.txt ]
+then
+  IKEV2_STATUS_FILE="/var/www/status/ikev2.txt"
+else
+    mkdir -p /var/www/status/
+    touch /var/www/status/ikev2.txt
+    IKEV2_STATUS_FILE="/var/www/status/ikev2.txt"
+fi
 
 # Check if the OpenVPN service is running
 if systemctl is-active openvpn.service > /dev/null 2>&1; then
